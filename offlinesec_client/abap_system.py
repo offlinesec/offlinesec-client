61cf4f29a75b0bfa058cb61e24b3a0a79111dc41
Offline Security <info.offlinesec@gmail.com>
2026-01-20 22:40:20 +0100

    sapui5+hana

--- offlinesec_client/abap_system.py
+++ offlinesec_client/abap_system.py
@@ -59,13 +59,11 @@ class ABAPSystem (SAPSystem):
         self.host_agent_patch = ABAPSystem.check_kernel_patch(args["host_agent_patch"]) \
             if "host_agent_patch" in args.keys() and args["host_agent_patch"] is not None and args["host_agent_patch"] != ""else ""
 
-        self.sapui5 = ABAPSystem.check_sapui5_ver(args["sapui5"]) \
-            if "sapui5" in args.keys() and args["sapui5"] is not None and \
-            args["sapui5"] != "" else ""
+        self.sapui5 = args["sapui5"] if "sapui5" in args.keys() and args["sapui5"] is not None and \
+            args["sapui5"] != "" and ABAPSystem.check_sapui5_ver(args["sapui5"]) else ""
 
-        self.hana = ABAPSystem.check_hana_ver(args["hana"]) \
-            if "hana" in args.keys() and args["hana"] is not None and \
-            args["hana"] != "" else ""
+        self.hana = args["hana"] if "hana" in args.keys() and args["hana"] is not None and \
+            args["hana"] != "" and ABAPSystem.check_hana_ver(args["hana"]) else ""
 
     @staticmethod
     def check_soft_line(soft, version, pkg_num, package):
